4decdff0505edf39d56a7673afd8fcbe5042eec2
Edward LoPinto <e.e.lopinto@gmail.com>
2016-12-15 21:18:57 -0500

    Set global tint

--- Cloudy/Application Delegate/AppDelegate.swift
+++ Cloudy/Application Delegate/AppDelegate.swift
@@ -13,10 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Configure Window
-        window?.tintColor = UIColor(red:0.99, green:0.47, blue:0.44, alpha:1.0)
-
+    func application(_ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
         return true
     }
 
